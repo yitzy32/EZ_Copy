47513dabee67fd5ff55a571132ff67bd9dead775
yitzy32 <yitzybratt32@gmail.com>
2023-02-01 22:38:30 -0500

    modify get_totals. upgrade to python3

--- index.py
+++ index.py
@@ -5,12 +5,21 @@ def invalid_dir(path):
         print('Sorry. '+ path +' is not a valid directory')
         return True 
 
-def get_total(folder, ext):
-    count = 0
-    for file_name in os.listdir(folder):
+def get_total(source, dest, ext):
+    totals = {}
+    to_copy = set()
+    to_skip = set()
+    for file_name in os.listdir(source):
         if file_name.endswith(ext):
-            count+=1
-    return count
+            to_copy.add(file_name)
+    for file_name in os.listdir(dest):
+        if file_name.endswith(ext):
+            to_copy.discard(file_name)
+            to_skip.add(file_name)
+    totals['to_copy'] = len(to_copy)
+    totals['to_skip'] = len(to_skip)
+    return totals
+
 def calc_percentage(val, totalVal):
     return str(int(val/totalVal * 100)) + "%"
 
@@ -23,7 +32,7 @@ def get_file_extensions(path):
 
 def get_inputs(): 
     inputs = {}
-    source = raw_input('Please provide full path of folder to copy from\n').strip()
+    source = input('Please provide full path of folder to copy from\n').strip()
     if invalid_dir(source): return inputs
     extension_options = get_file_extensions(source)
     if len(extension_options) == 0:
@@ -35,18 +44,26 @@ def get_inputs():
             options += ext
             options += ", "
         options += '\nPlease type bellow the extension you would like to copy.\n'
-    ext = raw_input(options)
+    ext = input(options)
     if ext not in extension_options:
         print('Sorry. '+ext+' is an invalid file extension.')
         return inputs
-    dest = raw_input('You chose to copy all '+ext+' files. \nNow please provide full path of folder to copy to\n').strip()
+    dest = input('You chose to copy all '+ext+' files. \nNow please provide full path of folder to copy to\n').strip()
     if invalid_dir(dest): return inputs
-    total = get_total(source, ext)
-    confirm = raw_input('\nAre you sure you want to copy all '+str(total)+' '+ext+' files from\n'+ source +'\nto \n' + dest +'? \n Enter Y/N\n')
+    totals = get_total(source, dest, ext)
+    if totals['to_copy'] == 0:
+        print('No new files found to copy.')
+        return inputs
+    message = ''
+    if totals['to_skip'] > 0:
+        message += 'Found '+ str(totals['to_skip']) + ' files that already exist in ' + dest + '. Those files will be skipped.'
+    message += '\nAre you sure you want to copy all '+str(totals['to_copy'])+' '+ext+' files from\n'+ source +'\nto \n' + dest +'? \n Enter Y/N\n'
+    confirm = input(message)
     inputs["source"] = source
     inputs["ext"] = ext
     inputs["dest"] = dest
-    inputs["total"] = total
+    inputs["total_copy"] = totals['to_copy']
+    inputs["total_skip"] = totals['to_skip']
     inputs["confirm"] = confirm
     return inputs
 
@@ -54,7 +71,7 @@ def copy(inputs):
     source = inputs["source"]
     ext = inputs["ext"]
     dest = inputs["dest"]
-    total = inputs["total"]
+    total_to_copy = inputs["total_copy"]
     total_copied, total_skipped = 0, 0
     for file_name in os.listdir(source):
         if file_name.endswith(ext):
@@ -63,14 +80,14 @@ def copy(inputs):
                 total_skipped += 1
             else: 
                 total_copied += 1
-                print('Copying '+file_name+'. '+str(total_copied)+ ' of '+ str(total)+' ('+calc_percentage(total_copied, total)+' done)')
+                print('Copying '+file_name+'. '+str(total_copied)+ ' of '+ str(total_to_copy)+' ('+calc_percentage(total_copied, total_to_copy)+' done)')
                 os.popen('cp "'+source+'/'+file_name+ '" "' + dest+'/'+file_name+'"')
 
     print('All Done! \nCopied: '+ str(total_copied) +' files. Skipped: '+ str(total_skipped) +' files')
 
 def process():
     inputs = get_inputs()
-    if('confirm' not in inputs):
+    if('confirm' not in inputs or inputs['confirm'].lower() == 'n'):
         print('Exiting.')
         return 
     if(inputs["confirm"].lower() == 'y'):
